cb097ed3d5ccdb3d997979d9a014b59cea984797
LucasMorais1998 <lucas14.morais@gmail.com>
2023-10-03 15:13:44 -0300

    listAllUsers: fix execute on ListAllUsersUseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -11,8 +11,8 @@ class ListAllUsersUseCase {
   execute({ user_id }: IRequest): User[] {
     const user = this.usersRepository.findById(user_id);
 
-    if (!user.admin) {
-      throw new Error("Access denied");
+    if (!user || !user.admin) {
+      throw new Error("You need to be an administrator to list all users");
     }
 
     const users = this.usersRepository.list();
